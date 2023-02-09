8361ccff3f5a3401d3db77ad35a1289dfc7a5118
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-09 18:56:34 +0200

    1 done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function pow(x, n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+let x = prompt('Введіть число, яке потрібно піднести до степеню:');
+let n = prompt('Введіть степінь, до якої бажаєте піднести число:');
+
+if (n < 1) {
+    alert(`Ступінь ${n} невірна,введіть натуральне число`);
+} else {
+    alert(pow(x, n));
+}
